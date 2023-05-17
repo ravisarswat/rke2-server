be05746dedb025e260b645952c2a268e5d4ae489
ravisarswat <39032339+ravisarswat@users.noreply.github.com>
2023-05-17 11:32:08 +0530

    Update install_rancher.sh

--- install_rancher.sh
+++ install_rancher.sh
@@ -4,14 +4,14 @@ cp /etc/rancher/rke2/rke2.yaml .kube/config
 
 export KUBECONFIG=/etc/rancher/rke2/rke2.yaml  PATH=$PATH:/var/lib/rancher/rke2/bin
 
-curl -#L https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+#curl -#L https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+#helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 
-helm repo add jetstack https://charts.jetstack.io
+#helm repo add jetstack https://charts.jetstack.io
 
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.crds.yaml
+#kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.crds.yaml
 
-helm upgrade -i cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace
+#helm upgrade -i cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace
 
-helm upgrade -i rancher rancher-latest/rancher --create-namespace --namespace cattle-system --set hostname=ryan.dell.com --set bootstrapPassword=ryan@123 --set replicas=1
+#helm upgrade -i rancher rancher-latest/rancher --create-namespace --namespace cattle-system --set hostname=ryan.dell.com --set bootstrapPassword=ryan@123 --set replicas=1
